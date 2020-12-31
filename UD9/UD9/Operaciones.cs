3d7aa9f54228d4687823ea2705a24d47ab38eeb9
Steelnox <eric14pastor@gmail.com>
2020-12-31 10:27:50 +0100

    Ejercicio 3 realizado (creación clase Operaciones)

---
+++ UD9/UD9/Operaciones.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD9
+{
+    public class Operaciones
+    {
+        private int numero1;
+        private int numero2;
+
+        public Operaciones(int numero1, int numero2)
+        {
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+        }
+
+        public int Numero1 { get => numero1; set => numero1 = value; }
+        public int Numero2 { get => numero2; set => numero2 = value; }
+    
+        private int Suma()
+        {
+            return numero1 + numero2;
+        }
+
+        private int Resta()
+        {
+            return numero1 - numero2;
+        }
+
+        private int Multiplicacion()
+        {
+            return numero1 * numero2;
+        }
+
+        private int Division()
+        {
+            return numero1 / numero2;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El resultado de la suma es " + Suma());
+            Console.WriteLine("El resultado de la resta es " + Resta());
+            Console.WriteLine("El resultado de la multiplicacion es " + Multiplicacion());
+            Console.WriteLine("El resultado de la division es " + Division());
+
+        }
+    }
+}
--- UD9/UD9/Program.cs
+++ UD9/UD9/Program.cs
@@ -8,6 +8,7 @@ namespace UD9
         {
             Alumno alum = new Alumno("Eric", 27);
             Empleado emplea = new Empleado("Andreu", 3500);
+            Operaciones opera = new Operaciones(10, 2);
 
             string opcion = "";
             Console.WriteLine("Escoja un numero de ejercicio entre el 1 y el 10 (ambos incluidos) ");
@@ -19,6 +20,9 @@ namespace UD9
                     break;
                 case "2":
                     emplea.UsoMetodos();
+                    break;
+                case "3":
+                    opera.Imprimir();
                     break;
                 default:
                     Console.WriteLine("Esa opción no funciona. Vuelve a intentarlo más tarde");
